b8cccd768b7a98e749068a8ed1c168b805ec19d0
Siles <fernando.siles@nokia.com>
2019-06-04 17:40:43 -0300

    Initial Commit

---
+++ CMakeProject1.cpp
@@ -0,0 +1,24 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "CMakeProject1.h"
+#include ".\\include\8080Emulator.h"
+
+using namespace std;
+
+int main() {
+
+	Cpu state(false);
+
+	char path[250];
+	cout << "Set rom path:" << endl;
+	cin >> path;
+
+	state.EmulateROM(path);
+	 
+	return 0;
+}
